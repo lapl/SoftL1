ad24f70b8e542a26b05e84fa33f510c087990dba
Lucas Lima <lapl@cin.ufpe.br>
2013-06-06 22:04:04 -0300

    Mudanças Q3

--- Q3.asm
+++ Q3.asm
@@ -1,34 +1,40 @@
-SECTION .data
+; alguns defines úteis do linux. 
+sys_exit        equ     1
+sys_read        equ     3
+sys_write       equ     4
 
-SECTION .bss
+stdin           equ     0
+stdout          equ     1
+stderr          equ     3
+
+	SECTION .bss
 
 string: resb 20
 
-SECTION .text
+	SECTION .text
 
 	global _start
 
 _start:
+	mov eax, sys_read
+	mov ebx, stdin
+	mov ecx, string
+	mov edx, 21 		;21 por causa do \n
+	int 0X80
 
-mov eax, 3
-mov ebx, 0
-mov ecx, string
-mov edx, 21
-int 0X80
-
-mov edi, 1
+	mov edi, 1
 
 imprime:
-	mov eax, 4
-	mov ebx, 1
+	mov eax, sys_write
+	mov ebx, stdout
 	int 0X80
 	
 	inc edi
 	cmp byte[ecx+edi], 0
 	jnz imprime
-	jz acaba
+	jz final
 
-acaba:
+final:
 	mov eax, 1 
 	mov ebx, 0
 	int 0X80
